6b7d4f22a93114e0a1baad7c0f0d87e53439e86a
jim <lucas@pgmkiun.com>
2026-05-26 14:11:39 +0700

    feat: 添加网站图标

--- src/views/home/step5.ts
+++ src/views/home/step5.ts
@@ -2,13 +2,15 @@ import bgPc from "@/static/home/step5/bg_pc.png";
 import bgH5 from "@/static/home/step5/bg_h5.png";
 import iconPc from "@/static/home/step5/icon_pc.png";
 import iconH5 from "@/static/home/step5/icon_h5.png";
+import router from "@/router";
 
 export function useStep5() {
   const joinBgPc = `url('${bgPc}')`;
   const joinBgH5 = `url('${bgH5}')`;
 
+  // 开启财富时代
   const handleJoin = () => {
-    console.log("开启财富时代");
+    router.push("/connect");
   };
 
   return {
